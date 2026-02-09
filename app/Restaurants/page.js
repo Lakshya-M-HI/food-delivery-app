28ebbd2af17bec93c222a101c1dd32965999719f
Lakshya-M-HI <lakshyamittal171@gmail.com>
2026-02-09 22:23:31 +0530

    clear errors

--- app/Restaurants/page.js
+++ app/Restaurants/page.js
@@ -17,35 +17,31 @@ export default function RestaurantGrid() {
     )
 
 
-return (
-    <>
+    return (
+        <>
 
-        <section className="relative px-4 md:px-8 py-8 ">
+            <section className="relative px-4 md:px-8 py-8 ">
 
-            {/* SEARCH BAR */}
-            <div className="max-w-xl mx-auto mb-8 ">
-                <div className="flex items-center bg-gray-200 rounded-xl shadow-md border border-gray-300 overflow-hidden">
-                    <input
-                        type="text"
-                        placeholder="Search restaurants, dish or location…"
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
-                        className="
-              flex-1
-              px-5 py-3
-              outline-none
-              text-sm md:text-base
+                {/* SEARCH BAR */}
+                <div className="max-w-xl mx-auto mb-8 ">
+                    <div className="flex items-center bg-gray-200 rounded-xl shadow-md border border-gray-300 overflow-hidden">
+                        <input
+                            type="text"
+                            placeholder="Search restaurants, dish or location…"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="flex-1 px-5 py-3 outline-none text-sm md:text-base
             "
-                    />
-                    <button className="px-6 text-[#FC8A06] font-semibold">
-                        Search
-                    </button>
+                        />
+                        <button className="px-6 text-[#FC8A06] font-semibold">
+                            Search
+                        </button>
+                    </div>
                 </div>
-            </div>
 
-            {/* GRID */}
-            <div
-                className="
+                {/* GRID */}
+                <div
+                    className="
                                 grid
                                 grid-cols-1
                                 sm:grid-cols-2
@@ -53,11 +49,11 @@ return (
                                 xl:grid-cols-4
                                 gap-6
                                 "
-            >
-                {filteredrestaurants.map((item) => (
-                    <div
-                        key={item.id}
-                        className="relative z-20 
+                >
+                    {filteredrestaurants.map((item) => (
+                        <div
+                            key={item.id}
+                            className="relative z-20 
                                             group p-3
                                             bg-white
                                             rounded-2xl
@@ -68,19 +64,19 @@ return (
                                             hover:shadow-xl
                                             hover:-translate-y-1
                                             "
-                    >
-                        {/* IMAGE */}
-                        <div className="absolute inset-0 -z-10 h-[180px] overflow-hidden">
-                            <Image
-                                src="/assets/360_F_758266271_yksdY7RLYIIY6jofjJorjylEDOvgcuze.jpg"
-                                alt={item.name}
-                                fill
-                                className="object-cover transition-transform duration-500 group-hover:scale-110"
-                            />
+                        >
+                            {/* IMAGE */}
+                            <div className="absolute inset-0 -z-10 h-[180px] overflow-hidden">
+                                <Image
+                                    src="/assets/360_F_758266271_yksdY7RLYIIY6jofjJorjylEDOvgcuze.jpg"
+                                    alt={item.name}
+                                    fill
+                                    className="object-cover transition-transform duration-500 group-hover:scale-110"
+                                />
 
-                            {/* SHADOW + BLUR LAYER */}
-                            <div
-                                className="
+                                {/* SHADOW + BLUR LAYER */}
+                                <div
+                                    className="
                                     absolute inset-0
                                     bg-linear-to-t
                                     from-black/80
@@ -88,12 +84,12 @@ return (
                                     to-black/10
                                     backdrop-blur-[2px]
                                     "
-                            />
+                                />
 
-                            {/* OFFER BADGE */}
-                            {item.offers && (
-                                <span
-                                    className="
+                                {/* OFFER BADGE */}
+                                {item.offers && (
+                                    <span
+                                        className="
                                             absolute bottom-0 right-0
                                             bg-[#FC8A06]
                                             text-white text-xs font-semibold
@@ -101,72 +97,72 @@ return (
                                             shadow-md
                                             z-10
                                         "
-                                >
-                                    {item.offers}
-                                </span>
-                            )}
-                        </div>
-
-
-                        {/* CONTENT */}
-                        <div className="p-4 space-y-2 relative z-20 text-white">
-
-                            {/* NAME + RATING */}
-                            <div className="flex items-center justify-between">
-                                <div>
-                                    <h3 className="font-semibold text-lg leading-tight">
-                                        {item.name}
-                                    </h3>
-                                </div>
-                                <span className="text-sm font-semibold text-green-600">
-                                    ⭐ {item.rating}
-                                </span>
-                            </div>
-
-                            {/* CUISINE */}
-                            <p className="text-sm text-gray-300 truncate">
-                                {item.cuisine.join(', ')},
-                                <span className='px-2'>{item.speciality}</span>
-                            </p>
-
-                            {/* META INFO */}
-                            <div className="flex items-center justify-between text-sm text-gray-300 mt-2">
-                                <span>{item.deliveryTime}</span>
-                                <span>{item.distance}</span>
-                                <span>Starting from ₹{(item.priceForTwo / 2) * 10}</span>
-                            </div>
-
-                            {/* FOOTER */}
-                            <div className="flex items-center justify-between mt-3 mb-3">
-                                <span
-                                    className={`text-xs font-medium px-3 py-1 rounded-full ${item.isOpen
-                                        ? 'bg-green-100 text-green-700'
-                                        : 'bg-red-100 text-red-600'
-                                        }`}
-                                >
-                                    {item.isOpen ? 'Open Now' : 'Closed'}
-                                </span>
-
-                                {item.isVeg && (
-                                    <span className="text-xs font-medium text-green-700">
-                                        🟢 Veg
+                                    >
+                                        {item.offers}
                                     </span>
                                 )}
                             </div>
 
+
+                            {/* CONTENT */}
+                            <div className="p-4 space-y-2 relative z-20 text-white">
+
+                                {/* NAME + RATING */}
+                                <div className="flex items-center justify-between">
+                                    <div>
+                                        <h3 className="font-semibold text-lg leading-tight">
+                                            {item.name}
+                                        </h3>
+                                    </div>
+                                    <span className="text-sm font-semibold text-green-600">
+                                        ⭐ {item.rating}
+                                    </span>
+                                </div>
+
+                                {/* CUISINE */}
+                                <p className="text-sm text-gray-300 truncate">
+                                    {item.cuisine.join(', ')},
+                                    <span className='px-2'>{item.speciality}</span>
+                                </p>
+
+                                {/* META INFO */}
+                                <div className="flex items-center justify-between text-sm text-gray-300 mt-2">
+                                    <span>{item.deliveryTime}</span>
+                                    <span>{item.distance}</span>
+                                    <span>Starting from ₹{(item.priceForTwo / 2) * 10}</span>
+                                </div>
+
+                                {/* FOOTER */}
+                                <div className="flex items-center justify-between mt-3 mb-3">
+                                    <span
+                                        className={`text-xs font-medium px-3 py-1 rounded-full ${item.isOpen
+                                            ? 'bg-green-100 text-green-700'
+                                            : 'bg-red-100 text-red-600'
+                                            }`}
+                                    >
+                                        {item.isOpen ? 'Open Now' : 'Closed'}
+                                    </span>
+
+                                    {item.isVeg && (
+                                        <span className="text-xs font-medium text-green-700">
+                                            🟢 Veg
+                                        </span>
+                                    )}
+                                </div>
+
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
 
-            {/* EMPTY STATE */}
-            {filteredrestaurants.length === 0 && (
-                <p className="text-center text-gray-500 mt-12">
-                    No restaurants found 😔
-                </p>
-            )}
+                {/* EMPTY STATE */}
+                {filteredrestaurants.length === 0 && (
+                    <p className="text-center text-gray-500 mt-12">
+                        No restaurants found 😔
+                    </p>
+                )}
 
-        </section>
-    </>
-)
+            </section>
+        </>
+    )
 }
